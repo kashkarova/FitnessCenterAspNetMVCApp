f140a79334c16b2fc59eb5cf804af8be5500135b
Tanya <Tanya@TANYA-PC>
2016-12-01 01:37:01 +0200

    First view 'CoachView' for adding data has been added.

---
+++ FitnessCenterAspNetMVCApp/Controllers/CoachController.cs
@@ -0,0 +1,25 @@
+﻿using FitnessCenterAspNetMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitnessCenterAspNetMVCApp.Controllers
+{
+    public class CoachController : Controller
+    {
+        // GET: Coach
+
+        DB_FitnessCenterWebEntities db = new DB_FitnessCenterWebEntities();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Coach> coaches = db.Coach;
+
+            ViewBag.Coach = coaches;
+
+            return View();
+        }
+    }
+}
