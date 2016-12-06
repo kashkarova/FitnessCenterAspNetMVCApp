67358a4a65eea99e048db60ae87b1c10fb83588c
kashkarova <kashkarovatany@gmail.com>
2016-12-07 01:34:21 +0200

    Controller for services has been fixed. ServiceView has been added, but it works separatelty

---
+++ FitnessCenterAspNetMVCApp/Controllers/ServiceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitnessCenterAspNetMVCApp.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+
+        private DbModel db = new DbModel();
+        public ActionResult ServiceView()
+        {
+            ViewBag.serviceList = db.Service.ToList();
+
+            return View();
+        }
+    }
+}
