3e5d373e8362516ed51e31c571c50d7a3445c50b
kashkarova <kashkarovatany@gmail.com>
2016-12-07 03:06:44 +0200

    Controller for showing feedback with view has been added.

---
+++ FitnessCenterAspNetMVCApp/Controllers/FeedbackController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitnessCenterAspNetMVCApp.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private DbModel db = new DbModel();
+        
+        // GET: Feedback
+        public ActionResult AddFeedback()
+        {
+            var feedback = new Feedback();
+
+            return View(feedback);
+        }
+
+        public ActionResult AddFeedback(Feedback fb)
+        {
+
+            return View(fb);
+        }
+
+        public ActionResult ShowFeedback()
+        {
+            ViewBag.listFeedback = db.Feedback.ToList();
+
+            return View();
+        }
+    }
+}
